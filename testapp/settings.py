3bd8f51d5763d3009c1bedabf69a2cf585749b27
amir-rz <mr.amir.rezazadehh@gmail.com>
2022-05-10 15:32:06 +0000

    Added dockerfile

--- testapp/settings.py
+++ testapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lbosb870wag_^h=&8$b64sy#^)egqw#+sbw8rp4*b*%wuwmqig
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["185.231.59.40","http://185.231.59.40","localhost"]
 
 
 # Application definition
